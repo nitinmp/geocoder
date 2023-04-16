a6a56e4bdc07f690f26bf602338634ae88a170f2
thevedp <thevedp@gmail.com>
2023-04-16 23:13:01 +0530

    Download.py updated

--- download.py
+++ download.py
@@ -1,20 +1,16 @@
-import os
-import s3fs
-from flask import Flask, Response
+from flask import Flask, send_file
+import awswrangler as wr
 
 app = Flask(__name__)
 
 @app.route('/download')
 def download_file():
     bucket_name = 'zono-geocoder'
-    key = 'output/output.csv'
-    s3 = s3fs.S3FileSystem(anon=False)
-
-    if s3.exists(f"{bucket_name}/{key}"):
-        file_stream = s3.open(f"{bucket_name}/{key}", 'rb')
-        return Response(file_stream, mimetype='text/csv', headers={"Content-Disposition": "attachment;filename=output.csv"})
-    else:
-        return "File not found on S3"
+    object_key = 'output/output.csv'
+    file_name = 'output.csv'
+    file_path = f'/tmp/{file_name}'
+    wr.s3.download(path=f's3://{bucket_name}/{object_key}', local_file=file_path)
+    return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
     app.run()
