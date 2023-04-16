8bae6ef99eb1a6be3abe4932c756667225c6c9fb
thevedp <thevedp@gmail.com>
2023-04-16 12:38:33 +0530

    aws integrated

--- app.py
+++ app.py
@@ -2,15 +2,19 @@ import csv
 from geopy.geocoders import GoogleV3
 from flask import Flask, jsonify
 import pandas as pd
+import s3fs
 
 app = Flask(__name__)
 
 @app.route('/geocode')
 def geocode():
+    aws_access_key_id = "AKIA4CMSZ232GY35CEMV"
+    aws_secret_access_key = "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"
     api_key = "AIzaSyDQYZ4uHQ86UoLdUoNN2nMVuZ9EgGngDqg"
+    
     geolocator = GoogleV3(api_key = api_key)
 
-    df = pd.read_csv("./geocoding.csv")
+    df = pd.read_csv("s3://zono-geocoder/input/geocoding.csv")
     print(df)
 
     for i, row in df.iterrows():
@@ -25,7 +29,7 @@ def geocode():
             df.at [i, "Lat"] = location.latitude
             df.at [i, "Lon"] = location.longitude
             print(location.address)
-    df.to_csv("output.csv",index = False)
+    df.to_csv("s3://zono-geocoder/output/output.csv",index = False)
     return jsonify({'message': "Geocoding completed successfully"})
 if __name__ == '__main__':
     app.run()
