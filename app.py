663cda3a9070b1dbc5b0429c2d705ca96c1d96e6
thevedp <thevedp@gmail.com>
2023-04-17 18:15:01 +0530

    updated app.py file

--- app.py
+++ app.py
@@ -5,19 +5,20 @@ import pandas as pd
 # import s3fs
 import awswrangler as wr
 
+
 app = Flask(__name__)
 
 
-@app.route('/geocode')
+@app.route('/geo')
 def geocode():
-    aws_access_key_id = "AKIA4CMSZ232GY35CEMV"
-    aws_secret_access_key = "1ZQk2lOfSGYcF3P9clsN12FOk6BADbIOa3akVApV"
+    # aws_access_key_id = "YOUR_ACCESS_KEY_ID"
+    # aws_secret_access_key = "YOUR_SECRET_ACCESS_KEY"
     api_key = "AIzaSyDQYZ4uHQ86UoLdUoNN2nMVuZ9EgGngDqg"
 
     geolocator = GoogleV3(api_key=api_key)
 
-    # df = pd.read_csv("s3://zono-geocoder/input/geocoding.csv")
-    df = wr.s3.read_csv("s3://zono-geocoder/input/geocoding.csv")
+    df = pd.read_csv("geocoding.csv")
+    # df = wr.s3.read_csv("s3://zono-geocoder/input/geocoding.csv")
     print(df)
 
     for i, row in df.iterrows():
@@ -31,10 +32,23 @@ def geocode():
             df.at[i, "Address"] = location.address
             df.at[i, "Lat"] = location.latitude
             df.at[i, "Lon"] = location.longitude
+            
+            for component in location.raw['address_components']:
+                if 'locality' in component['types']:
+                    df.at[i, "City"] = component['long_name']
+                elif 'administrative_area_level_2' in component['types']:
+                    df.at[i, "District"] = component['long_name']
+                elif 'administrative_area_level_1' in component['types']:
+                    df.at[i, "State"] = component['long_name']
+                elif 'country' in component['types']:
+                    df.at[i, "Country"] = component['long_name']
+                elif 'postal_code' in component['types']:
+                    df.at[i, "Pincode"] = component['long_name']
+            
             print(location.address)
-    # df.to_csv("s3://zono-geocoder/output/output.csv", index=False)
-    wr.s3.to_csv(df, "s3://zono-geocoder/output/output.csv")
-
+    df.to_csv("test.csv", index=False)
+    # wr.s3.to_csv(df, "s3://zono-geocoder/output/output.csv")
     return jsonify({'message': "Geocoding completed successfully"})
+
 if __name__ == '__main__':
     app.run(debug=True)
