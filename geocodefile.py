77ecee456693c288866d51faed16448a547959fa
thevedp <thevedp@gmail.com>
2023-04-24 22:30:05 +0530

    updated

--- geocodefile.py
+++ geocodefile.py
@@ -2,7 +2,7 @@ from geopy.geocoders import GoogleV3
 from flask import Flask, jsonify, request, redirect
 import pandas as pd
 import awswrangler as wr
-
+import time
 def process():
     aws_access_key_id = "AKIA4CMSZ232GY35CEMV"
     aws_secret_access_key = "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"
@@ -13,18 +13,16 @@ def process():
     # df = pd.read_csv("geocoding.csv")
     df = wr.s3.read_csv("s3://zono-geocoder/input/" + filename)
     # print(df)
-
+    start_time = time.time()
     for i, row in df.iterrows():
-        lat = row["Lat"]
-        lon = row["Lon"]
-        print("Latitude: ", lat)
-        print("Longitude: ", lon)
+        lat = row["Botree_Lat"]
+        lon = row["Botree_Long"]
         location = geolocator.reverse(f"{lat}, {lon}")
 
         if location:
             df.at[i, "Address"] = location.address
-            df.at[i, "Lat"] = location.latitude
-            df.at[i, "Lon"] = location.longitude
+            df.at[i, "Botree_Lat"] = location.latitude
+            df.at[i, "Botree_Long"] = location.longitude
             
             for component in location.raw['address_components']:
                 if 'locality' in component['types']:
@@ -41,7 +39,9 @@ def process():
             # print(location.address)
     # df.to_csv("test.csv", index=False)
     # wr.s3.to_csv(df, "s3://zono-geocoder/output/output.csv")
+    elapsed_time = time.time() - start_time
     output_file = "output/" + filename.split("/")[-1]
     wr.s3.to_csv(df, f"s3://zono-geocoder/{output_file}")
+    print("Total Time: ",elapsed_time)
     # return jsonify({'message': "Geocoding completed successfully"})
     return redirect('/get_download')
